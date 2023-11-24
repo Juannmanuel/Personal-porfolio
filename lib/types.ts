1dfa331f60fb758834a2cf78f671d5362c8fcd70
Juan Manuel <losredondos551@gmail.com>
2023-11-24 05:59:58 -0300

    website2

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,11 @@
 import { links } from "./data";
 
 export type SectionName = (typeof links)[number]["name"];
+
+export type ProjectProps = {
+  title: string;
+  description: string;
+  tags: string[];
+  imageUrl: string;
+  website: string;  // Agrega esta línea
+};
