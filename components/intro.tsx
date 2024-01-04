9ad63fcd6507b0d3983a1763d69b90d5800c4411
Juan Manuel <losredondos551@gmail.com>
2024-01-04 13:35:53 -0300

    tipeo

--- components/intro.tsx
+++ components/intro.tsx
@@ -68,8 +68,8 @@ export default function Intro() {
         <span className="font-bold">Hola, mi nombre es Juan Manuel.</span> Soy{" "}
         <span className="font-bold">desarollador full-stack.</span>{" "}Mi{" "}
         <span className="font-bold">trabajo es</span>{" "}
-        <span className="underline">convertir ideas en</span>
-        <span className="underline">experiencias de usuarios</span>.
+        <span className="underline">convertir ideas en experiencias de usuarios</span>
+
       </motion.h1>
 
       <motion.div
